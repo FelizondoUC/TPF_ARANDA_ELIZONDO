3ecd4be00dd595cd774d1169dc9e0bc4b461efe5
FelizondoUC <146740017+FelizondoUC@users.noreply.github.com>
2023-10-30 09:39:05 -0300

    Update computadora.c

--- computadora.c
+++ computadora.c
@@ -5,30 +5,12 @@
  *      Author: lp1-2023
  */
 
-// Player1, tiene dados del 1 al 6
-#define PLAYER1     1
-// Player2, tiene dados del 7 al 12
-#define PLAYER2     2
-// Tableros, principal y espejo para evaluación
-#define MAINBOARD   1
-#define MIRRORBOARD 2
-// Valor inicial mejor jugada.
-#define MENOS_INFINITO  -20000
-#define VACIO       0
-#define TAM_MAX     5
-// Valores booleanos
-#define TRUE        1
-#define FALSE       0
-// Modo de juego de IA
-#define AZAR_IA         1
-#define FIRSTSPACE_IA   2
-#define INTELIGENTE          3
-//Tablero del juego
-extern int board[TAM_MAX][TAM_MAX];
-//Tablero espejo que se usa para evaluar jugadas
-extern int mirror[TAM_MAX][TAM_MAX];
-// modojuego define quienes juegan. Util que sea externo para cuando se come, ya que si es PC vs PC, se come automáticamente sin consultar posiciones.
-
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "variables.h"
+#include "tablero.h"
+#include "jugabilidad.h"
 
 // ESTRATEGIA ALEATORIA
 // Cuando juega la PC, lo hace en forma aleatoria y carga en azarX,azarY la posición donde juega
